d33f4947a756a622bc4f958429583a0117de6f15
Gareth Johns <info@garethjohnsdesign.com>
2019-07-08 08:40:00 +0100

    Initial Commit

--- web/js/app.js
+++ web/js/app.js
@@ -9,57 +9,99 @@
 
 
 $(document).ready(function() {
-  function init() {
 
-    // 1. Foundation
-    // -------------
+// 1. Foundation
+// -------------
 
-    Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
-    Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
-    Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-width: 75em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
-    Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min-width: 90em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
-    $(document).foundation();
+Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-width: 75em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min-width: 90em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
+
+$(document).foundation();
 
 
-    $(window).scroll(function() {
-    	if ($(window).scrollTop() > 100 ){
-     		$('.navigation').addClass('show');
-      } else {
-        $('.navigation').removeClass('show');
-     	};   	
-    });
+// 2. Cookies Policy
+// -----------------
 
-    // 2. Animate on Scroll
-    // --------------------
+/*
+$(function() {
+  if($.cookie('showed_modal') !== "true") {
 
-    $(function() {
-      AOS.init({
-        offset: 64,
-        easing: 'ease-in-out-quart',
-        duration: 600
-      });
-    });
-    $(function() {
-      window.addEventListener('load', AOS.refresh);
-    });
+setTimeout(
+  function() 
+  {
+$("#cookiesPolicy").foundation("open");
+  }, 2000);
+
+    $.cookie('showed_modal', 'true', { expires: 365 }); 
   }
+});
+*/
 
-    // 3. Run Once
-    // -----------
+// 2. Animate on Scroll
+// --------------------
 
-    init();
+$(function() {
+  AOS.init({
+    offset: 64,
+    easing: 'ease-in-out-quart',
+    duration: 600
+  });
+});
 
-    // 4. Animations
-    // -------------
+$(function() {
+  window.addEventListener('load', AOS.refresh);
+});
 
-    var swup = new Swup({
-      plugins: [
-        swupGaPlugin
-      ],
-      animationSelector: '[class*="swup-transition-"]',
-      elements: ['#main']
+// 2. Carousel
+// -----------
+
+$(function() {
+
+    var firstSlide = Math.floor(Math.random() * 5);
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+
+      direction: 'vertical',
+      slidesPerView: 'auto',
+      centeredSlides: true,
+//       initialSlide: firstSlide,
+      initialSlide: 0,
+      loop: true,
+      simulateTouch: true,
+      mousewheel: true,
+      mousewheel: {
+        invert: false,
+      },
+      speed: 1200,
+      threshold: 15,
+      loop: true,
+/*
+      autoplay: {
+        delay: 4000,
+      },
+*/
+      preventClicks: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+
+/*
+  pagination: {
+      el: ".swiper-pagination-numbers",
+      type: "fraction"
+  },
+*/
+
+    navigation: {
+      nextEl: '.swiper-slide',
+    }
+
+   
     });
-    swup.on('contentReplaced', function() {
-      init();
-    });
+  });
+
+
 });
